ef8756695f068a41a7b2a7db9e4bfbacd82f3c25
mokunshao <mokunshao@foxmail.com>
2020-11-02 00:55:41 +0800

    backup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log("服务已启动");
