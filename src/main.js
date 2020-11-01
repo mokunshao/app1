a6c45b461d3dd5b883abc1d49bb9d986a1eadee5
mokunshao <mokunshao@foxmail.com>
2020-11-01 23:04:20 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log('服务已启动');
+  console.log("服务已启动");
 });
 
-app.get('/',()=>{
-  
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
