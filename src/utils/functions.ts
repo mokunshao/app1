78fc566105bccfb3c8df35763ea885760a2cbc60
mokunshao <mokunshao@foxmail.com>
2020-11-15 17:37:52 +0800

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,2 +1,2 @@
-export const removeObjectUndefined = (obj: { [key: string]: Object }): void =>
+export const removeObjectUndefined = (obj: { [key: string]: unknown }): void =>
   Object.keys(obj).forEach((key) => obj[key] === undefined && delete obj[key]);
