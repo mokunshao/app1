d109ea9e7be245fba17e8e4fa85e6f5a6ef22716
mokunshao <mokunshao@foxmail.com>
2021-10-07 20:44:55 +0800

    backup

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { postController } from "./post.controller";
 import { appMiddleware } from "../app/app.middleware";
-import { authGuard } from "../auth/auth.middleware";
+import { authGuard, accessControl } from "../auth/auth.middleware";
 
 export const postRouter = express.Router();
 
@@ -9,7 +9,13 @@ postRouter.get("/posts", appMiddleware.logRequestURL, postController.index);
 postRouter.post("/posts", authGuard, postController.store);
 postRouter.patch(
   "/posts/:postId",
-  appMiddleware.logRequestData,
+  authGuard,
+  accessControl({ possession: true }),
   postController.update
 );
-postRouter.delete("/posts/:postId", postController.destroy);
+postRouter.delete(
+  "/posts/:postId",
+  authGuard,
+  accessControl({ possession: true }),
+  postController.destroy
+);
