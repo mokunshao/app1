4782d7b26b04ce384451492124254a8911b542de
mokunshao <mokunshao@foxmail.com>
2021-10-10 20:35:35 +0800

    backup

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,13 +3,14 @@ import { postRouter } from "../post/post.router";
 import { appMiddleware } from "../app/app.middleware";
 import { userRouter } from "../user/user.router";
 import authRouter from "../auth/auth.router";
+import fileRouter from "../file/file.router";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(postRouter, userRouter, authRouter);
+app.use(postRouter, userRouter, authRouter, fileRouter);
 
 app.use(appMiddleware.defaultErrorHandler);
 
